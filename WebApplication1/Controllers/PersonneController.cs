a984a46a378beb57a8666705db9fb622ebbe1cae
lenovo <lenovo@DESKTOP-27G2AKJ>
2023-07-13 16:48:09 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/PersonneController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonneController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly DateTime BirthDate;
+
+        public PersonneController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Personne> objPersonList = _db.Personnes;   
+            return View(objPersonList);
+        }
+        //GET
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public IActionResult Add(Personne obj)
+        {
+            if (Personne.Age(BirthDate) >= 150 && Personne.Age(BirthDate) < 0)
+            {
+                ModelState.AddModelError("Custom Error", "Birthdate invalid");
+            }
+            if (ModelState.IsValid) { 
+            _db.Personnes.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index","Personne");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ WebApplication1/Models/Personne.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Personne
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Prename { get; set; }
+
+        public DateTime BirthDate { get; set ; }
+
+        //public Personne(int id, string name, string prename, DateTime birthDate)
+        //{
+        //    Id = id;
+
+        //    Name = name;
+
+        //    Prename = prename;
+
+        //    BirthDate = birthDate.Date;
+
+        //}
+
+        public  int Age (DateTime BirthDate)
+        {
+            var age=Convert.ToInt32(DateTime.Now.Year)-Convert.ToInt32(BirthDate.Year);
+            return age;
+        }
+
+        //internal static int Age(object birthDate)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+   
+    
+}
